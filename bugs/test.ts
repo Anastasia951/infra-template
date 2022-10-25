13f036e99fe2f668737729e5d8020dab270dfa10
Anastasia951 <n.ivanova504@gmail.com>
2022-10-26 00:24:29 +0300

    4

--- bugs/test.ts
+++ bugs/test.ts
@@ -1,15 +1,2 @@
-type SomethingComplex = {
-  foo: number
-  bar: string
-}
-function takeSomethingComplex(arg: SomethingComplex) {}
-function getBar(): string {
-  return 'some bar'
-}
-
-const fail = {
-  foo: 123,
-  bar: getBar,
-}
-
-takeSomethingComplex(fail) // TS ОШИБКА ПРОИСХОДИТ ЗДЕСЬ
+const message = 111
+message.toLowerCase()
