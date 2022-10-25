e7a84f96580e9339f9044a4e1aa10c3e1a9ede4f
Anastasia951 <n.ivanova504@gmail.com>
2022-10-26 00:14:31 +0300

    ts error_3

--- bugs/test.ts
+++ bugs/test.ts
@@ -1,2 +1,15 @@
-let one_thousand: number = 1000
-console.log(one_thousand.split('0'))
+type SomethingComplex = {
+  foo: number
+  bar: string
+}
+function takeSomethingComplex(arg: SomethingComplex) {}
+function getBar(): string {
+  return 'some bar'
+}
+
+const fail = {
+  foo: 123,
+  bar: getBar,
+}
+
+takeSomethingComplex(fail) // TS ОШИБКА ПРОИСХОДИТ ЗДЕСЬ
