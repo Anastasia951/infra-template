b7baaefc28abf0903e68d0964f680bb02bba9495
Anastasia951 <n.ivanova504@gmail.com>
2022-10-26 18:29:25 +0300

    comments

--- ci/tracker.js
+++ ci/tracker.js
@@ -1,6 +1,10 @@
 import * as github from '@actions/github'
-import * as core from '@actions/core'
+import { exec } from 'child_process'
+import { promisify } from 'util';
 import fetch from 'node-fetch'
+
+
+const execPromised = promisify(exec)
 const { OAUTH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
 
 const headers = {
@@ -9,18 +13,18 @@ const headers = {
   'X-Org-ID': X_ORG_ID
 }
 
-const patchTicketURL = `https://api.tracker.yandex.net/v2/issues/${TICKET_ID}`
-
 function formatTodayDate() {
   const today = new Date()
   return new Intl.DateTimeFormat('ru-RU').format(today)
 }
 
 export async function updateTicket() {
+  const patchTicketURL = `https://api.tracker.yandex.net/v2/issues/${TICKET_ID}`
   const regexp = /rc-0.0.\d+$/
   const tag = github.context.ref
   const author = github.context.actor
   const version = tag.match(regexp)[0]
+  const maintenance = tag.match(regexp)[1]
 
   try {
     await fetch(patchTicketURL, {
@@ -32,9 +36,34 @@ export async function updateTicket() {
       })
     })
     console.log("Тикет успешно создан")
+
+    await createComment(maintenance)
+    console.log("Комментарий успешно создан")
+
   } catch (e) {
     console.log("Ошибка при создании тикета")
   }
 }
+async function getCommitsBetweenTags(currentTag) {
+  const gitLogCommand = `git log --pretty="format:%an - %H" ${currentTag === 1 ? "...rc-0.0.1" : `rc-0.0.${currentTag - 1}...rc-0.0.${currentTag}`}`
+  const { stdout: logs } = await execPromised(gitLogCommand)
 
+  return logs
+}
+async function createComment(currentTag) {
+  const pathCommentsURL = `https://api.tracker.yandex.net/v2/issues/${TICKET_ID}/comments`
+  try {
+    const text = await getCommitsBetweenTags(currentTag)
+    await fetch(pathCommentsURL, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        text,
+      })
+    })
+    console.log("Комментарий успешно создан")
+  } catch (e) {
+    console.log("Ошибка при создании комментария")
+  }
+}
 updateTicket()
