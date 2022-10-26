997135c01420329485079e46fe9908b7bc63855b
Anastasia951 <n.ivanova504@gmail.com>
2022-10-26 17:04:06 +0300

    tru

--- ci/tracker.js
+++ ci/tracker.js
@@ -3,7 +3,7 @@ import * as core from '@actions/core'
 const { OUATH_TOKEN, TICKET_ID, X_ORG_ID } = process.env
 
 export async function updateTicket() {
-  const regexp = /rc-0.0.(<maintenance>\d+)/
+  const regexp = /rc-0.0.(?<maintenance>\d+)/
   const tag = github.context.ref
   core.info(tag)
   const { maintenance } = tag.match(regexp)
